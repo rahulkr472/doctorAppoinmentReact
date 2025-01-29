acb53d319be8275a0e3fad69ef2d5464435dc5a5
rahul <rahulkrkumar.98786@gmail.com>
2025-01-29 18:36:39 +0530

    11th update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-  
-    chunkSizeWarningLimit: 1500 // Increase the limit (in KB)
-  },
 })
